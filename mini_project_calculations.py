07e103994ce6f383475dea0ff339ef52a41bd5bf
k-samoilova <kateryna.samoilova.25@kse.org.ua>
2026-07-22 16:49:44 +0300

    calculate short and long moving average for 1 week and 1 month period of time accordingly, create cross over strategy

--- mini_project_calculations.py
+++ mini_project_calculations.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 TSM = yf.Ticker("TSM")
 data = TSM.history(period="1y")
 print(data[["Open", "Close", "High", "Low", "Volume"]])
+
+data["ShortMovAvg"] = data["Close"].rolling(7).mean()
+data["LongMovAvg"] = data["Close"].rolling(28).mean()
+
+data["Signal"] = 0
+data.loc[data["ShortMovAvg"] > data["LongMovAvg"], "Signal"] = 1
+
+print(data)
